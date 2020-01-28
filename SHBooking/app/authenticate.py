a6d8749d7d61c8ad1ce24a93061e5909ba2baaee
bijaydangol <bijaydangol11@gmail.com>
2020-01-28 15:27:45 +0545

    authentication

--- SHBooking/app/authenticate.py
+++ SHBooking/app/authenticate.py
@@ -1,12 +1,12 @@
 from django.shortcuts import redirect
 from app.models import User
 from django.contrib import messages
+from django.db.models import Q
 class Authenticate:
 	def valid_user(function):
 		def wrap(request):
 			try:
-				User.objects.get(email=request.session['email'])
-				User.objects.get(password=request.session['password'])
+				user=User.objects.get(Q(email=request.session['email']) & Q(password=request.session['password']))
 				return function(request)
 			except:
 				messages.warning(request,'please login first')
